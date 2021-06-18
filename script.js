45d4b5f62702076f7d4b4b5b71b894897dd43a20
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-06-18 18:42:27 -0500

    Comments for DEV

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
+   //move json fetch
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       event.preventDefault();
@@ -7,30 +8,34 @@ window.addEventListener("load", function() {
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass]");
-      
+      //setup variables for faultyItems 
+      //let element = document.getElementById("element-id");
       if (pilotNameInput.value   === "" || 
           copilotNameInput.value === "" ||
           fuelLevelInput.value   === "" ||
           cargoMassInput.value   === "") {
          alert("All fields are required!");
       } 
-      console.log(pilotNameInput.value);
-      console.log(copilotNameInput.value);   
-      console.log(fuelLevelInput.value);
-      console.log(cargoMassInput.value);         
+//combine if statement and when all true, 
+//let faultyItems = document.getElementById("faultyItems");
+//faultyItems.style.visibility="visible";        
       //true if the given value is NOT NaN 
       if (!isNaN(pilotNameInput.value) || 
           !isNan(copilotNameInput.value) ) {
          alert("Pilot and Co-Pilot names must not be numeric!");
-      } 
+      } else{
+
+      }
       //true if the given value is NaN
       if (isNaN(fuelLevelInput.value) || 
           isNan(cargoMassInput.value) ) {
          alert("Fuel level and cargo mass must be numeric!");
-      }          
+      }
+
    });
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
+//move to top so it loads once
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
